c93ea9255b8e5df2bc6f13dc83ef131c2864aa02
bjgribb <bjgribb@gmail.com>
2020-02-17 16:28:56 -0500

    added interface for type error

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
-import { environment } from "../../environments/environment";
-import { map } from "rxjs/operators";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { map } from 'rxjs/operators';
 
 //"https://accounts.spotify.com/authorize?client_id=9d1bb998f8a64aa69c40289b96af33eb&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fcallback%2F&scope=user-read-private%20user-read-email&state=34fFs29kd09",
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class SpotifyService {
   clientId: string = environment.clientId;
@@ -15,17 +15,14 @@ export class SpotifyService {
   constructor(private http: HttpClient) {}
 
   getAuth() {
-    const baseURL = "https://accounts.spotify.com/authorize";
+    const baseURL = 'https://accounts.spotify.com/authorize';
 
     const httpOptions = {
       params: new HttpParams()
-        .set("client_id", this.clientId)
-        .set("response_type", "token")
-        .set("redirect_uri", "http://localhost:4200/callback/")
-        .set(
-          "scope",
-          "user-read-private, user-read-email, playlist-read-private, playlist-read-collaborative"
-        )
+        .set('client_id', this.clientId)
+        .set('response_type', 'token')
+        .set('redirect_uri', 'http://localhost:4200/callback/')
+        .set('scope', 'user-read-private, user-read-email, playlist-read-private, playlist-read-collaborative')
     };
 
     // const httpOptions = {
@@ -47,8 +44,13 @@ export class SpotifyService {
       headers: { Authorization: `Bearer ${token}` }
     };
 
-    const basePlaylistURL = "https://api.spotify.com/v1/me/playlists";
+    const basePlaylistURL = 'https://api.spotify.com/v1/me/playlists';
 
-    return this.http.get(basePlaylistURL, httpOptions);
+    return this.http.get<PlaylistData>(basePlaylistURL, httpOptions);
   }
+}
+
+interface PlaylistData {
+  href: string;
+  items: any[];
 }
